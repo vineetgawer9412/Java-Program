2eb1ab2ed78dc1610941b513446d96ffe554a34d
vineetgawer9412 <vineetgawer9@gmail.com>
2025-08-12 14:49:09 +0530

    Update hello.java
    1st program

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
-public class hello{
-    public static void main(String[] args){
-        System.out.println("Hello world");
+import java.util.*;
+class main {
+    public static void main(String[] args) {
+        
     }
 }
